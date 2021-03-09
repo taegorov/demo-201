81d1ab43d6f074712a4e902f58a58f914be12307
taegorov <taegorov@gmail.com>
2021-03-09 13:42:01 -0800

    adds biography section to my HTML

---
+++ js/app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// 3 data types - string, number, boolean, array
+let a = 'some text';
+let b = 4;
+let c = true;
+//shows in console log what type of data type you're using:
+console.log(typeof(c)); 
+
+let d = [1,4,2,'Timba',7,4,5,true,false];
+
+
+// conditionals
+// let age = prompt('STATE YOUR AGE');
+
+// if (age >= 18) {
+//     alert('go vote!');
+// } else {
+//     alert('too young to vote sry')
+// }
+
+// switch(age){
+//     case('17'):
+//         alert('you can almost vote!');
+//         break;
+//     case('18'):
+//         alert('you can barely vote!');
+//         break;
+//     case('19'):
+//         alert('you\'ve been able to vote for a year!');
+//         break;
+//     default:
+//         alert('you\'re either too young to vote, or have been voting a while');
+// }
+
+
+// alert('mixed Case TEXT'.toLowerCase());
