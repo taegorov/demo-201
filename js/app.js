75811abddde5aa47615e5ce307840430f72d3d6c
taegorov <taegorov@gmail.com>
2021-03-09 15:27:40 -0800

    adds javascript guessing game and asks for users name

--- js/app.js
+++ js/app.js
@@ -1,37 +1,63 @@
 'use strict'
 
-// 3 data types - string, number, boolean, array
-let a = 'some text';
-let b = 4;
-let c = true;
-//shows in console log what type of data type you're using:
-console.log(typeof(c)); 
+function getUserName(){
+    let userName = prompt('Please enter your name');
+    if (userName == 'Tim'){
+        alert ('I knew it was you hehe');
+        document.write ('<h2 id="userNameTim">' + 'Welcome, You Beautiful Man' + '</h2>');
+    } else {
+        alert ('Thanks for answering all those questions, ' + userName + '!');
+        document.write ('<h2 id="otherNames">' + 'Welcome to My Humble Website, ' + userName + '</h2>');
+    }
+    return userName;
+}
 
-let d = [1,4,2,'Timba',7,4,5,true,false];
+let deadliftAmount = prompt('Is Tim\'s heaviest deadlift 515 pounds?').toLowerCase();
+if (deadliftAmount === 'yes') {
+    alert('That is correct!');
+} else if (deadliftAmount === 'no') {
+    alert('Wrong!');
+} else {
+    alert('That\'s not yes or no, but that\'s fine');
+}
+// console.log('answer to "is deadlift 515?" ' + deadliftAmount);
 
+let whichCountry = prompt('Was Tim born in the USA?').toLowerCase();
+if (whichCountry === 'no') {
+    alert('That is correct!');
+} else if (whichCountry === 'yes') {
+    alert('Wrong!');
+} else {
+    alert('That\'s not yes or no, but whatever I guess');
+}
+// console.log('answer to "born in USA?" ' + whichCountry);
 
-// conditionals
-// let age = prompt('STATE YOUR AGE');
+let whichUniversity = prompt('Did Tim go to University of Washington?').toLowerCase();
+if (whichUniversity === 'no') {
+    alert('That is correct!');
+} else if (whichUniversity === 'yes') {
+    alert('Wrong!');
+} else {
+    alert('That\'s not yes or no, but I\'m not your boss');
+}
+// console.log('answer to "go to UW?" ' + whichUniversity);
 
-// if (age >= 18) {
-//     alert('go vote!');
-// } else {
-//     alert('too young to vote sry')
-// }
+let whichIndustry = prompt('Did Tim come from the sales industry?').toLowerCase();
+if (whichIndustry === 'yes') {
+    alert('That is correct!');
+} else if (whichIndustry === 'no') {
+    alert('Wrong!');
+} else {
+    alert('That\'s not yes or no, but do your thing');
+}
+// console.log('answer to "come from sales?" ' + whichIndustry);
 
-// switch(age){
-//     case('17'):
-//         alert('you can almost vote!');
-//         break;
-//     case('18'):
-//         alert('you can barely vote!');
-//         break;
-//     case('19'):
-//         alert('you\'ve been able to vote for a year!');
-//         break;
-//     default:
-//         alert('you\'re either too young to vote, or have been voting a while');
-// }
-
-
-// alert('mixed Case TEXT'.toLowerCase());
+let whichVice = prompt('Will Tim have gone 1 year without alcohol in July?').toLowerCase();
+if (whichVice === 'no') {
+    alert('That is correct!');
+} else if (whichVice === 'yes') {
+    alert('Wrong!');
+} else {
+    alert('You were supposed to enter yes or no');
+}
+// console.log('answer to "year without alcohol?" ' + whichVice);
