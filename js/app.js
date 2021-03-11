8385c5ca3a89201140c613f5ec267f08d5a962cd
taegorov <taegorov@gmail.com>
2021-03-10 17:41:59 -0800

    adds border and background color to h1

--- js/app.js
+++ js/app.js
@@ -3,84 +3,72 @@
 //tracking correct answers
 let totalCorrectAnswers = 0;
 
-// function getUserName(){
-//     let userName = prompt('Please enter your name');
-//     if (userName == 'Tim'){
-//         alert ('I knew it was you hehe');
-//         document.write ('<h2 id="userNameTim">' + 'Welcome, You Beautiful Man' + '</h2>');
-//     } else {
-//         alert ('Thanks for answering all those questions, ' + userName + '!');
-//         document.write ('<h2 id="otherNames">' + 'Welcome to My Humble Website, ' + userName + '</h2>');
-//     }
-//     return userName;
-// }
+function getUserName(){
+    let userName = prompt('Please enter your name');
+    if (userName == 'Tim'){
+        alert ('I knew it was you hehe');
+        document.write ('<h2 id="userNameTim">' + 'Welcome, You Beautiful Man' + '</h2>');
+    } else {
+        alert ('Thanks for answering all those questions, ' + userName + '!');
+        document.write ('<h2 id="otherNames">' + 'Welcome to My Humble Website, ' + userName + '</h2>');
+    }
+    return userName;
+}
 
-// let deadliftAmount = prompt('Is Tim\'s heaviest deadlift 515 pounds?').toLowerCase();
-// if (deadliftAmount === 'yes') {
-//     alert('That is correct!');
-        totalCorrectAnswers += 1;
-// } else if (deadliftAmount === 'no') {
-//     alert('Wrong!');
-// } else {
-//     alert('That\'s not yes or no, but that\'s fine');
-// }
-// // console.log('answer to "is deadlift 515?" ' + deadliftAmount);
+let deadliftAmount = prompt('Is Tim\'s heaviest deadlift 515 pounds?').toLowerCase();
+if (deadliftAmount === 'yes') {
+    alert('That is correct!');
+    totalCorrectAnswers += 1;
+} else if (deadliftAmount === 'no') {
+    alert('Wrong!');
+} else {
+    alert('That\'s not yes or no, but that\'s fine');
+}
+// console.log('answer to "is deadlift 515?" ' + deadliftAmount);
 
-// let whichCountry = prompt('Was Tim born in the USA?').toLowerCase();
-// if (whichCountry === 'no') {
-//     alert('That is correct!');
-// } else if (whichCountry === 'yes') {
-//     alert('Wrong!');
-// } else {
-//     alert('That\'s not yes or no, but whatever I guess');
-// }
-// // console.log('answer to "born in USA?" ' + whichCountry);
+let whichCountry = prompt('Was Tim born in the USA?').toLowerCase();
+if (whichCountry === 'no') {
+    alert('That is correct!');
+    totalCorrectAnswers += 1;
+} else if (whichCountry === 'yes') {
+    alert('Wrong!');
+} else {
+    alert('That\'s not yes or no, but whatever I guess');
+}
+// console.log('answer to "born in USA?" ' + whichCountry);
 
-// let whichUniversity = prompt('Did Tim go to University of Washington?').toLowerCase();
-// if (whichUniversity === 'no') {
-//     alert('That is correct!');
-// } else if (whichUniversity === 'yes') {
-//     alert('Wrong!');
-// } else {
-//     alert('That\'s not yes or no, but I\'m not your boss');
-// }
-// // console.log('answer to "go to UW?" ' + whichUniversity);
+let whichUniversity = prompt('Did Tim go to University of Washington?').toLowerCase();
+if (whichUniversity === 'no') {
+    alert('That is correct!');
+    totalCorrectAnswers += 1;
+} else if (whichUniversity === 'yes') {
+    alert('Wrong!');
+} else {
+    alert('That\'s not yes or no, but I\'m not your boss');
+}
+// console.log('answer to "go to UW?" ' + whichUniversity);
 
-// let whichIndustry = prompt('Did Tim come from the sales industry?').toLowerCase();
-// if (whichIndustry === 'yes') {
-//     alert('That is correct!');
-// } else if (whichIndustry === 'no') {
-//     alert('Wrong!');
-// } else {
-//     alert('That\'s not yes or no, but do your thing');
-// }
-// // console.log('answer to "come from sales?" ' + whichIndustry);
+let whichIndustry = prompt('Did Tim come from the sales industry?').toLowerCase();
+if (whichIndustry === 'yes') {
+    alert('That is correct!');
+    totalCorrectAnswers += 1;
+} else if (whichIndustry === 'no') {
+    alert('Wrong!');
+} else {
+    alert('That\'s not yes or no, but do your thing');
+}
+// console.log('answer to "come from sales?" ' + whichIndustry);
 
-// let whichVice = prompt('Will Tim have gone 1 year without alcohol in July?').toLowerCase();
-// if (whichVice === 'no') {
-//     alert('That is correct!');
-// } else if (whichVice === 'yes') {
-//     alert('Wrong!');
-// } else {
-//     alert('You were supposed to enter yes or no');
-// }
-// // console.log('answer to "year without alcohol?" ' + whichVice);
-
-// let answer = 4;
-// let guesses = 5;
-// for (let i = 0; i < guesses; i+=1){
-//     let userAnswer = prompt ('Guess a number betweeen 1 and 10');
-//     if(userAnswer === answer) {
-//         alert('That\'s correct!');
-//         break;
-//     } else if (userAnswer < 4) {
-//         alert('Too low, try again!');
-//     } else if (userAnswer > 4) {
-//         alert('Too high, try again!');
-//     } else {
-//         alert('Must enter a number!');
-//     }
-// }
+let whichVice = prompt('Will Tim have gone 1 year without alcohol in July?').toLowerCase();
+if (whichVice === 'no') {
+    alert('That is correct!');
+    totalCorrectAnswers += 1;
+} else if (whichVice === 'yes') {
+    alert('Wrong!');
+} else {
+    alert('You were supposed to enter yes or no');
+}
+// console.log('answer to "year without alcohol?" ' + whichVice);
 
 //Number guessing game
 //Random number generator provided by Roger Huba from Code 102, who referenced w3 Schools
@@ -94,6 +82,7 @@ for (let i = 0; i < guesses; i+=1) {
     }
     if (userAnswer == correctAnswer) {
         alert('That\'s correct!');
+        totalCorrectAnswers += 1;
         break;
     } else if(i === 2 && userAnswer !== correctAnswer){
         alert('Wrong! One more guess!');
@@ -126,6 +115,7 @@ let flag = false;
         console.log(correctGuess[k], k);
         if(userGuess === correctGuess[k]) {
             alert('You are correct!!!');
+            totalCorrectAnswers += 1;
             i = 7;
             break;
         } else if(userGuess !== correctGuess[k] && k === correctGuess.length - 1 ){
@@ -138,3 +128,5 @@ let flag = false;
        alert('The correct numbers were ' + correctGuess);
  }
  }
+
+ alert('You answered ' + totalCorrectAnswers + ' out of 7 questions correctly!')
